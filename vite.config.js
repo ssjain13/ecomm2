82fe437dd13005d6efe491192d1606d8f08adf51
Chaplot <sonal.chaplot@accenture.com>
2023-03-16 00:12:56 +0530

    Updating base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/ecomm2",
   plugins: [react()],
   define: {
     "process.env": {
